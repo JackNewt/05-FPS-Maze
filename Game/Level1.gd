f35f1dac43c9eeb2cd41733c782392f9c9dbcf2d
JackNewt <122556891+JackNewt@users.noreply.github.com>
2023-04-09 22:47:44 -0400

    Update 0.5.3
    Enemies now die Efficiently

--- Game/Level1.gd
+++ Game/Level1.gd
@@ -1,14 +1,2 @@
 extends Spatial
 
-onready var Enemy = preload("res://Enemy/Enemy.tscn")
-
-func _ready():
-	var enemy = Enemy.instance()
-	add_child(enemy)
-	enemy.translate(Vector3(-20, .2, 4))
-	enemy = Enemy.instance()
-	add_child(enemy)
-	enemy.translate(Vector3(11, .2, -20))
-	enemy = Enemy.instance()
-	add_child(enemy)
-	enemy.translate(Vector3(2, .2, -13))
